9cf97f16cbc216de08ba88d3774d5af14b694a39
andrey-a-kiryakov <andrey.a.kiryakov@gmail.com>
2025-02-01 20:43:47 +0400

    iter2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,6 +12,7 @@ import (
 // curl -v -X POST -H "Content-Type:text/plain" -d "ya.ru" "http://localhost:8080"
 // curl -v -X GET -H "Content-Type:text/plain" "http://localhost:8080/GlTBlr"
 // shortenertest-windows-amd64 -test.v -test.run=^TestIteration1$ -binary-path=C:\go\shorturl\cmd\shortener\shortener.exe
+// C:\go\shorturl\cmd\shortener>go vet -vettool=C:\go\statictest-windows-amd64.exe ./...
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	//var l = storage.Link{URL: "https://habr.com/ru/articles/457728/"}
