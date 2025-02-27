58173138e4a0497c7c49d72571e7616e46c4f0de
d5kx <d5kx@ya.ru>
2025-02-27 09:45:40 +0400

    fix for tests

--- internal/app/adapters/http/handlers/base/handler.go
+++ internal/app/adapters/http/handlers/base/handler.go
@@ -44,7 +44,7 @@ func (h *Handler) Get(res http.ResponseWriter, req *http.Request) {
 }
 
 func (h *Handler) Post(res http.ResponseWriter, req *http.Request) {
-	if !h.checkContentType(req, "text/plain") {
+	if !h.checkContentType(req, "text/plain") && !h.checkContentType(req, "application/x-gzip") {
 		res.WriteHeader(http.StatusBadRequest)
 		return
 	}
